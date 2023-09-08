ef9201ca4eae53967bb301b2aee88d7f29a2544d
YuriSib <sib_94@vk.com>
2023-09-08 19:40:31 +0300

    Программа работает, но выдает множество ошибок, из-за которых данные собираются с пропусками. Предстоит проработать большое количество сценариев ошибок и поставить исключения туда, что не удастся проработать.

--- exchange_with_xl.py
+++ exchange_with_xl.py
@@ -7,7 +7,6 @@ def import_xl(stop, start=2):
     sheet = catalog.active
     list_qwery_ = []
 
-    count = 0
     #sheet.max_row + 1 в range - для вывода всего ряда (start, finish) - для диапазона
     for row in range(start, stop + 1):
         search_query = ''
@@ -17,12 +16,37 @@ def import_xl(stop, start=2):
 
         search_query = city + ' ' + place
         list_qwery_.append(search_query)
-        count += 1
+        start += 1
 
     return list_qwery_
 
 
-# запись в pickle
-list_qwery = import_xl(2991)
-with open('data.pickle', 'wb') as file:
-    pickle.dump(list_qwery, file)
+def export_xl(value1, value2, value3, value4, value5, value6, value7, row):
+    workbook = openpyxl.load_workbook('plases.xlsx')
+    sheet = workbook.active
+    sheet[row][2].value = value1
+    sheet[row][3].value = value2
+    sheet[row][4].value = value3
+    sheet[row][5].value = value4
+    sheet[row][6].value = value5
+    sheet[row][7].value = value6
+    sheet[row][8].value = value7
+    workbook.save('plases.xlsx')
+
+
+# list_qwery = import_xl(2991)
+# with open('data.pickle', 'wb') as file:
+#     pickle.dump(list_qwery, file)
+
+
+# with open('data.pickle', 'rb') as file:
+#     loaded_data = pickle.load(file)
+#
+# print(loaded_data)
+
+
+# export_xl('10:00', 2, 5)
+# export_xl('10:00', 3, 5)
+# export_xl('10:00', 4, 5)
+# export_xl('10:00', 5, 5)
+# export_xl('10:00', 6, 5)
--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 import time
 from bs4 import BeautifulSoup
+import pickle
 
-from exchange_with_xl import import_xl
+
+from exchange_with_xl import import_xl, export_xl
 
 
 def settings():
+    proxy = Proxy()
+    proxy.proxy_type = ProxyType.MANUAL
+    proxy.http_proxy = "http://1257421-all-country-DE:1wpldj8s1z@93.190.142.139:13736"
+    # proxy.ssl_proxy = "https://your_proxy_server:port"
     options = webdriver.ChromeOptions()
     options.add_argument("start-maximized")
 
-    # options.add_argument("--headless")
+
+    options.add_argument("--headless")
+
 
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
@@ -36,32 +45,130 @@ def settings():
 def scrapper(qwery):
     driver = settings()
     driver.get(url='https://yandex.ru/maps/')
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 20)
 
-    driver.find_element("xpath", "//span[@class='input__context']/input[@class='input__control _bold']").send_keys(qwery)
+    element = wait.until(EC.visibility_of_element_located(("xpath", "//span[@class='input__context']/input[@class='input__control _bold']")))
+    element.send_keys(qwery)
     ActionChains(driver).send_keys(Keys.RETURN).perform()
 
-    element = wait.until(EC.visibility_of_element_located(("xpath",
-                                                           '''//div[@class="search-business-snippet-view__title"]''')))
-    element.click()
+    time.sleep(10)
     html = driver.page_source
     soup = BeautifulSoup(html, 'lxml')
 
-    working_hours = soup.find('div', {'class': 'business-card-working-status-view__text'}).get_text(strip=True)
-    rating = soup.find('span', {'class': 'business-rating-badge-view__rating-text _size_m'}).get_text(strip=True)
-    qnt_ratings = soup.find('div', {'class': 'business-rating-with-text-view__count'}).get_text(strip=True).split()[0]
-    qnt_feedback = soup.find('div', {'class': 'business-header-rating-view__text _clickable'}).get_text(strip=True).split()[0]
+    price_html = soup.find('span', {'class': 'business-features-view__valued-value'})
+    if price_html:
+        price = price_html.get_text(strip=True).split()[0]
+    else:
+        price = "Не указана"
 
+    html_qnt_ratings = soup.find('div', {'class': 'business-card-view__section'})
+
+    try:
+        # choice1 = driver.find_element('xpath', '''/html/body/div[1]/div[2]/div[8]/div[2]/div[1]/div[1]/div[1]/div/div[1]/div/div/ul/li[1]/div/div/div/div[2]''')
+        choice2 = driver.find_element('xpath', '''/html/body/div[1]/div[2]/div[8]/div[2]/div[1]/div[1]/div[1]/div/div[1]/div/div/ul/li[1]/div/div/div''')
+        # if choice1:
+        #     element = choice1
+        #     element.click()
+        if choice2:
+            element = choice2
+        else:
+            element = driver.find_element("xpath", '''/html/body/div[1]/div[2]/div[8]/div[2]/div[1]/div[1]/div[1]/div/div[1]/div/div/ul/li[1]/div/div/div/div/div[2]/div[1]''')
+        element.click()
+        time.sleep(10)
+
+        coordinates = driver.current_url.split('=')[1].split('&')[0]
+        coordinates_a, coordinates_b = coordinates.split('%2C')[0], coordinates.split('%2C')[1]
+        coordinates = f'{coordinates_b}, {coordinates_a}'
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+
+        try:
+            qnt_ratings = soup.find('span', {'class': 'business-rating-amount-view'}).get_text(strip=True).split()[0]
+        except Exception:
+            pass
+
+        # working_hours = soup.find('div', {'class': 'business-card-working-status-view__text'}).get_text(strip=True)
+        working_hours = soup.find_all('div', {'class': 'business-contacts-view__block'})[2].\
+            find('div', {'class': 'card-feature-view__content'}).get_text(strip=True)
+
+        rating = soup.find('span', {'class': 'business-rating-badge-view__rating-text _size_m'}).get_text(strip=True)
+
+        qnt_feedback = soup.find('div', {'class': 'business-header-rating-view__text _clickable'}).get_text(strip=True).split()[0]
+        type_ = soup.find('a', {'class': 'business-card-title-view__category _outline'}).get_text(strip=True).split()[0]
+
+        element = driver.find_element("xpath",
+                                      '''/html/body/div[1]/div[2]/div[9]/div/div[1]/div[1]/div/div[1]/div/div/div[3]/div[5]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[3]''')
+        element.click()
+
+        time.sleep(5)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        if qnt_ratings is False:
+            try:
+                qnt_ratings = soup.find('div', {'class': 'business-summary-rating-badge-view__rating-count'}).get_text(strip=True).split()[0]
+            except Exception:
+                qnt_ratings = 'Не удалось извлечь оценки'
+
+    except Exception:
+        element = driver.find_element("xpath", '''/html/body/div[1]/div[2]/div[9]/div[1]/div[1]/div[1]/div/div[1]/div/div/div[3]/div[2]/div[2]/h1/a''')
+        element.click()
+        time.sleep(10)
+
+        coordinates = driver.current_url.split('=')[1].split('&')[0]
+        coordinates_a, coordinates_b = coordinates.split('%2C')[0], coordinates.split('%2C')[1]
+        coordinates = f'{coordinates_b}, {coordinates_a}'
+
+        html2 = driver.page_source
+        soup2 = BeautifulSoup(html2, 'lxml')
+
+        working_hours = soup2.find('div', {'class': 'orgpage-header-view__working-status'}).get_text(strip=True)
+        rating = soup2.find('div', {'class': 'business-rating-badge-view__rating'}).get_text(strip=True)
+        qnt_feedback = soup2.find('div', {'class': 'business-header-rating-view__text _clickable'}).get_text(strip=True).split()[0]
+        type_ = soup2.find_all('a', {'class': 'breadcrumbs-view__breadcrumb _outline'})[2].get_text(strip=True).split()[0]
+
+        element = driver.find_element("xpath", '''/html/body/div[1]/div[2]/div[9]/div[1]/div[1]/div[1]/div/div[1]/div/div[3]/div/div[14]/div/div/div[2]/div[2]/div/div/div/div[1]/div[3]''')
+        element.click()
+
+        time.sleep(5)
+        html2 = driver.page_source
+        soup2 = BeautifulSoup(html2, 'lxml')
+        qnt_ratings = \
+        soup2.find('div', {'class': 'business-summary-rating-badge-view__rating-count'}).get_text(strip=True).split()[0]
+
+    driver.close()
     driver.quit()
 
-    return working_hours, rating, qnt_ratings, qnt_feedback
+    return coordinates, type_, working_hours, rating, qnt_ratings, qnt_feedback, price
 
 
 def main():
-    qwery_list = import_xl(8, 6)
-    for qwery in qwery_list:
-        scrapper(qwery)
+    with open('data.pickle', 'rb') as file:
+        qwery_list = pickle.load(file)
+        row = 45
+    for qwery in qwery_list[43:]:
+        try:
+            if 'лица' not in qwery and 'ляж' not in qwery:
+                coordinates, type_, working_hours, rating, qnt_ratings, qnt_feedback, price = scrapper(qwery)
+            else:
+                print('Нельзя искать улицы')
+                raise Exception
+        except Exception:
+            print('Exception')
+            coordinates, type_, working_hours, rating, qnt_ratings, qnt_feedback, price = '0', '0', '0', '0', '0', '0', '0'
+
+        export_xl(coordinates, type_, working_hours, rating, qnt_ratings, qnt_feedback, price, row)
+        row += 1
+        print(row)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
